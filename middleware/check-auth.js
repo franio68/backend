38a9c2bdc9dbac26c7f7e0124f0b799d2401ad10
Francisco Dávila <fjhdavila@hotmail.com>
2024-10-22 18:48:29 +0100

    Modificación de Autenticador para....

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
+console.log("Comentario para modificar el fichero y hacer un commit");
 const autorizacion = (req, res, next) => {
-	try {
-		const token = req.headers.authorization.split(' ')[1]; // Autorization: 'bearer TOKEN'
-		if (!token) {
-			throw new Error('Fallo de autenticación 1');
-		}
-		decodedTOKEN = jwt.verify(token, 'clave_supermegasecreta');
-		req.userData = {
-			userId: decodedTOKEN.userId,
-		};
-		next();
-	} catch (err) {
-		const error = new Error('Fallo de autenticación 2');
-		error.code = 401;
-		return next(error);
-	}
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Autorization: 'bearer TOKEN'
+    if (!token) {
+      throw new Error("Fallo de autenticación 1");
+    }
+    decodedTOKEN = jwt.verify(token, "clave_supermegasecreta");
+    req.userData = {
+      userId: decodedTOKEN.userId,
+    };
+    next();
+  } catch (err) {
+    const error = new Error("Fallo de autenticación 2");
+    error.code = 401;
+    return next(error);
+  }
 };
 
 module.exports = autorizacion;
